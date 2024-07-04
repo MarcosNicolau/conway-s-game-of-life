2891f5d68af4f0e633b45f13334aed821fd0f4df
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-07-04 18:21:58 -0300

    generate cells data structure

--- src/main.rs
+++ src/main.rs
@@ -10,40 +10,76 @@ struct Cell {
     is_dead: bool,
 }
 
+type CellMatrix = Vec<Vec<Cell>>;
+
 struct Game {
-    cells: Vec<Cell>,
-    cell_size: i32,
+    cells: CellMatrix,
+    cell_size: f32,
 }
 
 impl Game {
     pub fn new() -> Self {
+        let cell_size = 5.0;
         Self {
-            cell_size: 10,
-            cells: vec![],
+            cell_size,
+            cells: Game::generate_cells(cell_size),
         }
     }
 
-    pub async fn start(&mut self) {
-        self.setup();
+    fn generate_cells(cell_size: f32) -> CellMatrix {
+        let mut matrix: CellMatrix = vec![];
+        let num_of_rows = screen_height() / cell_size;
+        let num_of_cols = screen_width() / cell_size;
 
+        (0..num_of_rows as i32).into_iter().for_each(|row_idx| {
+            let cells: Vec<Cell> = (0..num_of_cols as i32)
+                .into_iter()
+                .map(|col_idx| Cell {
+                    pos: Pos {
+                        x: col_idx as f32 * cell_size,
+                        y: row_idx as f32 * cell_size,
+                    },
+                    is_dead: false,
+                })
+                .collect();
+            matrix.push(cells);
+        });
+
+        return matrix;
+    }
+
+    pub async fn start(&mut self) {
         loop {
             clear_background(BLACK);
+            self.cells.iter().for_each(|el| {
+                el.iter().for_each(|el| {
+                    draw_rectangle(
+                        el.pos.x,
+                        el.pos.y,
+                        self.cell_size,
+                        self.cell_size,
+                        if el.is_dead { BLACK } else { WHITE },
+                    )
+                })
+            });
             next_frame().await;
         }
     }
 
-    fn setup(&mut self) {}
+    fn conf() -> Conf {
+        Conf {
+            window_title: "Conway's Game of Life".to_owned(),
+            fullscreen: false,
+            window_resizable: false,
+            window_height: 600,
+            window_width: 800,
+            ..Default::default()
+        }
+    }
 }
 
 fn conf() -> Conf {
-    Conf {
-        window_title: "Conway's Game of Life".to_owned(),
-        fullscreen: false,
-        window_resizable: false,
-        window_height: 600,
-        window_width: 800,
-        ..Default::default()
-    }
+    Game::conf()
 }
 
 #[macroquad::main(conf)]
